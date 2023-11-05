e85ff12574696c3c5fadacb441ae294cfcaca983
h0mbre <47577114+h0mbre@users.noreply.github.com>
2023-11-04 21:00:41 -0400

    Create test.c

---
+++ test.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+    printf("Argument count: %d\n", argc);
+    printf("Args:\n");
+    for (int i = 0; i < argc; i++) {
+        printf("   -%s\n", argv[i]);
+    }
+
+    size_t iters = 0;
+    while (1) {
+        printf("Test alive!\n");
+        sleep(1);
+        iters++;
+
+        if (iters > 5) { return 0; }
+    }
+}
