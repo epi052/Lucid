2bad1a0b43f141ff509e5a386935fa2be731ff0f
h0mbre <47577114+h0mbre@users.noreply.github.com>
2023-11-04 21:02:09 -0400

    Create err.rs

---
+++ src/err.rs
@@ -0,0 +1,19 @@
+/// A place-holder for perhaps a more detailed/robust error reporting system in
+/// the future
+
+#[derive(Debug)]
+pub struct LucidErr {
+    message: String,
+}
+
+impl LucidErr {
+    pub fn from(message: &str) -> Self {
+        LucidErr {
+            message: message.to_string(),
+        }
+    }
+
+    pub fn display(&self) {
+        println!("{}", self.message);
+    }
+}
